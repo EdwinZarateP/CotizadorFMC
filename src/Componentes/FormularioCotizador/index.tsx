158d80a3ac3a37a313a18ce899161d423faa1c33
EdwinZarateP <emzp1994@gmail.com>
2024-12-04 17:46:34 -0500

    se corrijio logica

--- src/Componentes/FormularioCotizador/index.tsx
+++ src/Componentes/FormularioCotizador/index.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { ContextoApp } from "../../Contexto/index";
 import municipios from "../Ciudades/municipios.json"; 
 import combinaciones from "../Combinaciones/combinaciones.json"; 
-import Swal from 'sweetalert2'; // Importa Swal
+import Swal from 'sweetalert2'; 
 import "./estilos.css";
 
 const FormularioCotizador: React.FC = () => {
@@ -23,7 +23,7 @@ const FormularioCotizador: React.FC = () => {
 
   const [cajas, setCajas] = useState<number>(0); 
   const [peso, setPeso] = useState<number>(0); 
-  const [volumen, setVolumen] = useState<number>(0.2); // Nuevo estado para volumen
+  const [volumen, setVolumen] = useState<number>(0.2); 
   const [sugerenciasOrigen, setSugerenciasOrigen] = useState<string[]>([]); 
   const [sugerenciasDestino, setSugerenciasDestino] = useState<string[]>([]); 
 
@@ -40,11 +40,10 @@ const FormularioCotizador: React.FC = () => {
       municipio.CIUDAD_DEPARTAMENTO.toLowerCase().includes(input.toLowerCase())
     );
     
-    // Solo aplicar el filtro de ciudadesPermitidas para el destino
     return listaFiltrada
       .map((municipio) => municipio.CIUDAD_DEPARTAMENTO)
-      .filter((ciudad) => esOrigen ? true : ciudadesPermitidas.includes(ciudad)) // Cambié aquí
-      .slice(0, 10); // Limitar a los primeros 10 resultados
+      .filter((ciudad) => esOrigen ? true : ciudadesPermitidas.includes(ciudad))
+      .slice(0, 10); 
   };
 
   const validarValor = (valor: string): number => Math.max(0, Number(valor));
@@ -57,35 +56,25 @@ const FormularioCotizador: React.FC = () => {
   };
 
   const factorPeso = 400;
-
-  // Actualización del cálculo de peso volumétrico usando el volumen
   const pesoVolumetrico = volumen ? volumen * factorPeso : 0;
-
-  const porcentajeDeclarado = valorDeclarado
-    ? Math.max(2500, valorDeclarado * 0.01)
-    : 2500;
-
-  const pesoFinal = Math.max(
-    pesoVolumetrico,
-    peso,
-    25
-  );
+  const costoManejo = valorDeclarado
+    ? Math.max(2500*cajas, valorDeclarado * 0.01)
+    : 2500*cajas;
+  const pesoFinal = Math.max(pesoVolumetrico, peso, 25*cajas);
 
   const manejarCotizacion = (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Lista de campos vacíos
     const camposVacios: string[] = [];
 
     if (!ciudadOrigen) camposVacios.push('Ciudad de origen');
     if (!ciudadDestino) camposVacios.push('Ciudad de destino');
-    if (!volumen) camposVacios.push('Volumen'); // Verificar volumen
+    if (!volumen) camposVacios.push('Volumen'); 
     if (!peso) camposVacios.push('Peso');
     if (!cajas) camposVacios.push('Cantidad de cajas');
     if (!valorDeclarado) camposVacios.push('Valor declarado');
 
     if (camposVacios.length > 0) {
-      // Mostrar alerta con los campos vacíos
       Swal.fire({
         icon: 'warning',
         title: 'Campos incompletos',
@@ -93,71 +82,66 @@ const FormularioCotizador: React.FC = () => {
       });
       return;
     }
-    
+
     const formatearMoneda = (valor: number) => {
-        // Redondea al múltiplo de 50 más cercano
-        const valorRedondeado = Math.round(valor / 50) * 50;
-      
-        // Formatea como moneda sin decimales
-        return new Intl.NumberFormat('es-CO', { 
-          style: 'currency', 
-          currency: 'COP',
-          minimumFractionDigits: 0, // Sin decimales
-          maximumFractionDigits: 0  // Sin decimales
-        }).format(valorRedondeado);
-      };      
+      const valorRedondeado = Math.round(valor / 50) * 50;
+      return new Intl.NumberFormat('es-CO', { 
+        style: 'currency', 
+        currency: 'COP',
+        minimumFractionDigits: 0, 
+        maximumFractionDigits: 0  
+      }).format(valorRedondeado);
+    };
 
     const origenDestinoConcatenado = `${ciudadOrigen}/${ciudadDestino}`;
     const costoKg = Number(obtenerCostoKg(origenDestinoConcatenado));
     const valorDeclaradoFormateado = formatearMoneda(valorDeclarado);
-    const flete=formatearMoneda(pesoFinal * costoKg + porcentajeDeclarado);
-    // Mostrar el resultado de la cotización en una ventana emergente (modal) con el botón de cerrar
+    const flete = formatearMoneda(pesoFinal * costoKg + costoManejo);
+
     Swal.fire({
-        icon: 'success',
-        title: 'Calculo de tu flete',
-        html: `
-          <strong>Origen/Destino:</strong> ${ciudadOrigen}/${ciudadDestino} <br>
-          <strong>Costo por kg:</strong> ${costoKg} <br>
-          <strong>Volumen:</strong> ${volumen} <br> <!-- Muestra el volumen -->
-          <strong>Peso digitado:</strong> ${peso} <br>
-          <strong>Volumetrico:</strong> ${pesoVolumetrico} <br>
-          <strong>Peso elegido:</strong> ${pesoFinal} <br>
-          <strong>Cajas:</strong> ${cajas} <br>
-          <strong>Valor declarado:</strong> ${valorDeclaradoFormateado} <br>
-          <strong>Costo manejo:</strong> ${porcentajeDeclarado} <br>
-          <strong>Flete:</strong> ${flete}
-        `,
-        showCancelButton: false,
-        confirmButtonText: 'Cerrar',
-      });      
+      icon: 'success',
+      title: 'Cálculo de tu flete',
+      html: `
+        <strong>Origen/Destino:</strong> ${ciudadOrigen}/${ciudadDestino} <br>
+        <strong>Costo por kg:</strong> ${costoKg} <br>
+        <strong>Volumen:</strong> ${volumen} <br>
+        <strong>Peso digitado:</strong> ${peso} <br>
+        <strong>Volumétrico:</strong> ${pesoVolumetrico} <br>
+        <strong>Peso elegido:</strong> ${pesoFinal} <br>
+        <strong>Cajas:</strong> ${cajas} <br>
+        <strong>Valor declarado:</strong> ${valorDeclaradoFormateado} <br>
+        <strong>Costo manejo:</strong> ${costoManejo} <br>
+        <strong>Flete:</strong> ${flete}
+      `,
+      showCancelButton: false,
+      confirmButtonText: 'Cerrar',
+    });      
   };
-  
 
   const limpiarFormulario = () => {
     setCiudadOrigen('');
     setCiudadDestino('');
-    setVolumen(0.2); // Resetear volumen
+    setVolumen(0.2); 
     setPeso(1);
     setCajas(1);
     setValorDeclarado(500000);
   };
 
   return (
-    <>
-      <form className="formulario-cotizador-contenedor" onSubmit={manejarCotizacion}>
-        {/* Ciudad Origen */}
-        <div className="formulario-cotizador-select">
-          <label htmlFor="ciudadOrigen">Ciudad de origen</label>
-          <input
-            id="ciudadOrigen"
-            type="text"
-            placeholder="Ciudad de origen"
-            value={ciudadOrigen}
-            onChange={(e) => {
-              setCiudadOrigen(e.target.value);
-              setSugerenciasOrigen(filtrarCiudades(e.target.value, true));
-            }}
-          />
+    <form className="formulario-cotizador-contenedor" onSubmit={manejarCotizacion}>
+      <div className="formulario-cotizador-select">
+        <label htmlFor="ciudadOrigen">Ciudad de origen</label>
+        <input
+          id="ciudadOrigen"
+          type="text"
+          placeholder="Ciudad de origen"
+          value={ciudadOrigen}
+          onChange={(e) => {
+            setCiudadOrigen(e.target.value);
+            setSugerenciasOrigen(filtrarCiudades(e.target.value, true));
+          }}
+        />
+        {ciudadOrigen && (
           <span
             className="icono-limpiar"
             onClick={() => {
@@ -167,36 +151,34 @@ const FormularioCotizador: React.FC = () => {
           >
             &times;
           </span>
-          {sugerenciasOrigen.length > 0 && (
-            <ul className="formulario-cotizador-sugerencias">
-              {sugerenciasOrigen.map((sugerencia, index) => (
-                <li
-                  key={index}
-                  onClick={() => {
-                    setCiudadOrigen(sugerencia);
-                    setSugerenciasOrigen([]);
-                  }}
-                >
-                  {sugerencia}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+        )}
+        {sugerenciasOrigen.length > 0 && (
+          <ul className="formulario-cotizador-sugerencias">
+            {sugerenciasOrigen.map((sugerencia, index) => (
+              <li key={index} onClick={() => {
+                setCiudadOrigen(sugerencia);
+                setSugerenciasOrigen([]);
+              }}>
+                {sugerencia}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
 
-        {/* Ciudad Destino */}
-        <div className="formulario-cotizador-select">
-          <label htmlFor="ciudadDestino">Ciudad de destino</label>
-          <input
-            id="ciudadDestino"
-            type="text"
-            placeholder="Ciudad de destino"
-            value={ciudadDestino}
-            onChange={(e) => {
-              setCiudadDestino(e.target.value);
-              setSugerenciasDestino(filtrarCiudades(e.target.value, false));
-            }}
-          />
+      <div className="formulario-cotizador-select">
+        <label htmlFor="ciudadDestino">Ciudad de destino</label>
+        <input
+          id="ciudadDestino"
+          type="text"
+          placeholder="Ciudad de destino"
+          value={ciudadDestino}
+          onChange={(e) => {
+            setCiudadDestino(e.target.value);
+            setSugerenciasDestino(filtrarCiudades(e.target.value, false));
+          }}
+        />
+        {ciudadDestino && (
           <span
             className="icono-limpiar"
             onClick={() => {
@@ -206,88 +188,75 @@ const FormularioCotizador: React.FC = () => {
           >
             &times;
           </span>
-          {sugerenciasDestino.length > 0 && (
-            <ul className="formulario-cotizador-sugerencias">
-              {sugerenciasDestino.map((sugerencia, index) => (
-                <li
-                  key={index}
-                  onClick={() => {
-                    setCiudadDestino(sugerencia);
-                    setSugerenciasDestino([]);
-                  }}
-                >
-                  {sugerencia}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+        )}
+        {sugerenciasDestino.length > 0 && (
+          <ul className="formulario-cotizador-sugerencias">
+            {sugerenciasDestino.map((sugerencia, index) => (
+              <li key={index} onClick={() => {
+                setCiudadDestino(sugerencia);
+                setSugerenciasDestino([]);
+              }}>
+                {sugerencia}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
 
-        {/* Volumen */}
-        <div className="formulario-cotizador-entradas-fila">
-          <div className="entrada-titulo">
-            <label htmlFor="volumen">Volumen m³ (use separador coma)</label>
-            <input
-              id="volumen"
-              type="number"
-              step="any"
-              placeholder="Volumen"
-              value={volumen}
-              onChange={(e) => setVolumen(Number(e.target.value))}
-            />
-          </div>
-        </div>
+      <div className="formulario-cotizador-entradas-fila">
+        <label htmlFor="volumen">Volumen m³ (use separador coma)</label>
+        <input
+          id="volumen"
+          type="number"
+          step="any"
+          placeholder="Volumen"
+          value={volumen}
+          onChange={(e) => setVolumen(Number(e.target.value))}
+        />
+      </div>
 
+      <div className="formulario-cotizador-entradas-fila">
+        <label htmlFor="peso">Peso (kg)</label>
+        <input
+          id="peso"
+          type="number"
+          placeholder="Peso"
+          value={peso}
+          onChange={(e) => setPeso(Number(e.target.value))}
+        />
+      </div>
 
-        {/* Peso */}
-        <div className="formulario-cotizador-entradas-fila">
-          <div className="entrada-titulo">
-            <label htmlFor="peso">Peso (kg)</label>
-            <input
-              id="peso"
-              type="number"
-              placeholder="Peso"
-              value={peso}
-              onChange={(e) => setPeso(Number(e.target.value))}
-            />
-          </div>
-        </div>
+      <div className="formulario-cotizador-entradas-fila">
+        <label htmlFor="cajas">Cantidad de cajas</label>
+        <input
+          id="cajas"
+          type="number"
+          value={cajas}
+          onChange={(e) => setCajas(Number(e.target.value))}
+        />
+      </div>
 
-        {/* Cajas */}
-        <div className="formulario-cotizador-entradas-fila">
-          <div className="entrada-titulo">
-            <label htmlFor="cajas">Cantidad de cajas</label>
-            <input
-              id="cajas"
-              type="number"
-              value={cajas}
-              onChange={(e) => setCajas(Number(e.target.value))}
-            />
-          </div>
-        </div>
+      <div className="formulario-cotizador-entradas-fila">
+        <label htmlFor="valorDeclarado">Valor declarado</label>
+        <input
+          id="valorDeclarado"
+          type="number"
+          value={valorDeclarado}
+          onChange={(e) => setValorDeclarado(validarValor(e.target.value))}
+        />
+      </div>
 
-        {/* Valor Declarado */}
-        <div className="formulario-cotizador-entradas-fila">
-          <div className="entrada-titulo">
-            <label htmlFor="valorDeclarado">Valor declarado</label>
-            <input
-              id="valorDeclarado"
-              type="number"
-              value={valorDeclarado}
-              onChange={(e) => setValorDeclarado(validarValor(e.target.value))}
-            />
-          </div>
-        </div>
-        <div className="formulario-cotizador-contenedor-botones">
-          <div className="formulario-cotizador-boton-calculo">
-            <button type="submit">Calcular Flete</button>
-          </div>
-          <div className="formulario-cotizador-boton-limpiar">
-            <button type="button" onClick={limpiarFormulario}>Limpiar formulario</button>
-          </div>
-        </div>
-      </form>
-    </>
+      <div className="formulario-cotizador-botones">
+        <button type="submit" className="formulario-cotizador-boton">Cotizar</button>
+        <button
+          type="button"
+          className="formulario-cotizador-boton limpiar"
+          onClick={limpiarFormulario}
+        >
+          Limpiar
+        </button>
+      </div>
+    </form>
   );
 };
 
